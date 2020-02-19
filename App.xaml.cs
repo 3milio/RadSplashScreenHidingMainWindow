0d695e5c3e02f3307277898b13b974efc933c4db
Emilio Ramirez <eramirez@r2cgroup.com>
2020-02-18 19:13:08 -0800

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using Telerik.Windows.Controls;
+
+namespace RadSplashScreenHidingMainWindow
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            RadSplashScreenManager.Show<CustomSplashScreen>();
+            Thread.Sleep(3000);
+            RadSplashScreenManager.Close();
+
+            base.OnStartup(e);
+        }
+    }
+}
